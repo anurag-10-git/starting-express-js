2d358b77d74cd0c47d9d8b274a2787e21c4a0bae
anurag-10-git <anuragmehra2011@gmail.com>
2022-12-03 02:55:15 +0530

    basics finished

--- app.js
+++ app.js
@@ -8,13 +8,14 @@ const adminRoute = require('./routes/admin')
 const shopRoute = require('./routes/shop');
 
 app.use(bodyParser.urlencoded({extended:false}))
+app.use(express.static(path.join(__dirname,'public')))
 
 app.use('/admin',adminRoute);
 
 app.use(shopRoute);
 
 app.use((req,res,next)=>{
-   res.status(404).sendFile(path.join(__dirname,'views','error.html'));
+   res.status(404).sendFile(path.join(__dirname,'views','error.html'));  
 })
 
 app.listen(3000);
--- routes/admin.js
+++ routes/admin.js
@@ -2,13 +2,17 @@ const express = require('express');
 const path = require('path');
 const router = express.Router();
 
+const rootDir = require('../util/path')
+
 router.get('/add-product',(req, res , next)=>{
     console.log('The add product page!');
-    res.sendFile(path.join(__dirname,'../','views','add-product.html'));
+    console.log(rootDir);
+    res.sendFile(path.join(rootDir,'views','add-product.html'));
  });
  
  router.post('/add-product',(req,res,next)=>{
     console.log(req.body);
     res.redirect('/');
  })
+
 module.exports = router;
